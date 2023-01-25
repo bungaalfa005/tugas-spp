3848ec33d2a72190bc19b9f71944c9a1c7ccfdaa
bungaalfa005 <109193542+bungaalfa005@users.noreply.github.com>
2023-01-25 14:50:01 +0700

    Add files via upload

---
+++ hapus-data.php
@@ -0,0 +1,13 @@
+<?php
+include("koneksi.php");
+isset($_GET['simpan']);
+     $kode = $_GET['id'];
+
+     $sql = "DELETE FROM tb_pasien WHERE id=$kode";
+     $query = mysqli_query($koneksi, $sql);
+
+     if($query){
+        header('location:pasien.php?status=sukses');
+     }else{
+        die ("akses dilarang");
+     }
